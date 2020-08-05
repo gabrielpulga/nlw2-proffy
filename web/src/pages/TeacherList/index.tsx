2a035d5bbae8d4f1b0b0bfe370a2dffe6551fa24
Gabriel Pulga <gabrieelplg@gmail.com>
2020-08-04 21:08:17 -0300

    new changes

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -4,22 +4,23 @@ import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import backIcon from '../../assets/images/icons/back.svg';
 
+import './styles.css';
+
 
 function TeacherList() {
     return (
-        
         <div id="page-teacher-list" className="container">
-            <header className="page">
-            <div className="top-bar-container">
-            <Link to="/">
-                <img src={backIcon} alt="Voltar"/>
-            </Link>
-            <img src={logoImg} alt="Proffy"/>
-            </div>
+            <header className="page-header">
+                <div className="top-bar-container">
+                    <Link to="/">
+                        <img src={backIcon} alt="Voltar" />
+                    </Link>
+                    <img src={logoImg} alt="Proffy" />
+                </div>
 
-            <div className="header-content">
-            <strong>Estes são os proffys disponíveis.</strong>
-            </div>
+                <div className="header-content">
+                    <strong>Estes são os proffys disponíveis.</strong>
+                </div>
             </header>
         </div>
     )
