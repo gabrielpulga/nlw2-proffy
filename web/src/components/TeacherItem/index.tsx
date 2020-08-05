9bd45a40c0aae44b855cd11fbdd4393b46e1e2fd
Gabriel Pulga <gabrieelplg@gmail.com>
2020-08-04 23:04:45 -0300

    day one

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+import './styles.css';
+
+function TeacherItem() {
+    return(
+        <article className="teacher-item">
+        <header>
+            <img src="https://avatars3.githubusercontent.com/u/61952539?s=460&v=4" alt="Gabriel Pulga"/>
+            <div>
+                <strong>Gabriel Pulga</strong>
+                <span>Matemática</span>
+            </div>
+            </header>
+            <p>
+                Estudante de engenharia, entusiasta de software.
+                <br/><br/>
+                Disponível para começar a dar aulas imediatamente.
+            </p>
+            <footer>
+                <p>
+                    Preço/hora
+                    <strong>R$80,00</strong>
+                </p>
+                <button type="button">
+                <img src={whatsappIcon} alt="Whatsapp"/>
+                Entrar em contato
+                </button>
+            </footer>
+            </article>
+    );
+}
+
+export default TeacherItem;
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,31 +1,42 @@
 import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import TeacherItem from '../../components/TeacherItem';
 
 
 function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
-        <PageHeader title="Estes são os proffys disponíveis.">
-            <form id="search-teachers">
-                <div className="input-block">
-                <label htmlFor="subject">Matérias</label>
-                <input type="text" id="subject" />
-                </div>
+            <PageHeader title="Estes são os proffys disponíveis.">
+                <form id="search-teachers">
 
-                <div className="input-block">
-                <label htmlFor="week-day">Dia da semana</label>
-                <input type="text" id="week-day" />
-                </div>
+                    <div className="input-block">
+                        <label htmlFor="subject">Matéria</label>
+                        <input type="text" id="subject" />
+                    </div>
 
-                <div className="input-block">
-                <label htmlFor="time">Hora</label>
-                <input type="text" id="time" />
-                </div>
-            </form>
-        </PageHeader>
+                    <div className="input-block">
+                        <label htmlFor="week-day">Dia da semana</label>
+                        <input type="text" id="week-day" />
+                    </div>
+
+                    <div className="input-block">
+                        <label htmlFor="time">Hora</label>
+                        <input type="text" id="time" />
+                    </div>
+
+                </form>
+            </PageHeader>
+
+            <main>
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+                <TeacherItem />
+            </main>
         </div>
     )
 }
