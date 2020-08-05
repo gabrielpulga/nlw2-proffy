d406dd2511b27514203f42e902427539bdb47bbf
Gabriel Pulga <gabrieelplg@gmail.com>
2020-08-04 21:36:03 -0300

    new changes

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-import logoImg from '../../assets/images/logo.svg';
-import backIcon from '../../assets/images/icons/back.svg';
+import PageHeader from '../../components/PageHeader';
 
 import './styles.css';
 
@@ -10,18 +8,24 @@ import './styles.css';
 function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
-            <header className="page-header">
-                <div className="top-bar-container">
-                    <Link to="/">
-                        <img src={backIcon} alt="Voltar" />
-                    </Link>
-                    <img src={logoImg} alt="Proffy" />
+        <PageHeader title="Estes são os proffys disponíveis.">
+            <form id="search-teachers">
+                <div className="input-block">
+                <label htmlFor="subject">Matérias</label>
+                <input type="text" id="subject" />
                 </div>
 
-                <div className="header-content">
-                    <strong>Estes são os proffys disponíveis.</strong>
+                <div className="input-block">
+                <label htmlFor="week-day">Dia da semana</label>
+                <input type="text" id="week-day" />
                 </div>
-            </header>
+
+                <div className="input-block">
+                <label htmlFor="time">Hora</label>
+                <input type="text" id="time" />
+                </div>
+            </form>
+        </PageHeader>
         </div>
     )
 }
