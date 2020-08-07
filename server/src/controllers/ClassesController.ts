3eaa5e0a43f8f822bfcda9d3921fa0d784c89c68
Gabriel Pulga <gabrieelplg@gmail.com>
2020-08-07 17:44:20 -0300

    class_schedule whereRaw's

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -14,7 +14,7 @@ export default class Classescontroller {
         const filters = request.query;
 
         const subject = filters.subject as string;
-        const week_day = filters.subject as string;
+        const week_day = filters.week_day as string;
         const time = filters.time as string;
 
         if(!filters.week_day || !filters.subject || !filters.time) {
@@ -26,7 +26,17 @@ export default class Classescontroller {
         const timeInMinutes = convertHourToMinuteS(time as string);
 
         const classes = await db('classes')
-            .where('classes.subject', '=', subject as string);
+        .whereExists(function(){
+            this.select('class_schedule.*')
+                .from('class_schedule')
+                .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+                .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+                .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+                .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+        })
+            .where('classes.subject', '=', subject as string)
+            .join('users', 'classes.user_id', '=', 'users.id')
+            .select(['classes.*', 'users.*']);
 
         return response.json(classes);
     }
