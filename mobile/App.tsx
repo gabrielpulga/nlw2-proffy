2146fdb5ec98a0ce72ae21014d69478e32b2f662
Gabriel Pulga <gabrieelplg@gmail.com>
2020-08-12 15:01:43 -0300

    53:06

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import Landing from './src/pages/Landing';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import Landing from "./src/pages/Landing";
+import { AppLoading } from "expo";
+import {
+  Archivo_400Regular,
+  Archivo_700Bold,
+  useFonts,
+} from "@expo-google-fonts/archivo";
+import {
+  Poppins_400Regular,
+  Poppins_600SemiBold,
+} from "@expo-google-fonts/poppins";
 
 export default function App() {
-  return (
-    <>
-    <Landing />
-    <StatusBar style="auto" />
-    </>
-  );
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <>
+        <Landing />
+        <StatusBar style="light" />
+      </>
+    );
+  }
 }
--- mobile/src/pages/Landing/styles.ts
+++ mobile/src/pages/Landing/styles.ts
@@ -5,9 +5,54 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#8257E5',
         justifyContent: 'center',
-        padding: 40;
+        padding: 40
     },
 
+    banner: {
+        width: '100%',
+        resizeMode: 'contain'
+    },
+
+    title: {
+        fontFamily: 'Poppins_400Regular',
+        color: '#FFF',
+        fontSize: 20,
+        lineHeight: 30,
+        marginTop: 80
+    },
+
+    titleBold: {
+        fontFamily: 'Poppins_600SemiBold'
+    },
+
+    buttonsContainer: {
+        flexDirection: 'row',
+        marginTop: 40,
+        justifyContent: 'space-between',
+    },
+
+    button: {
+        height: 150,
+        width: '48%',
+        backgroundColor: '#333',
+        borderRadius: 8,
+        padding: 24,
+        justifyContent: 'space-between'
+    },
+
+    buttonPrimary: {
+        backgroundColor: '#9871f5',
+    },
+
+    buttonSecondary: {
+        backgroundColor: '#04d361'
+    },
+
+    buttonText: {
+        fontFamily: 'Archivo_700Bold',
+        color: '#FFF',
+        fontSize: 20
+    }
 });
 
 export default styles;
