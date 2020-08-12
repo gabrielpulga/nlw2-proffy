bcd62f7547c9ff2d7ff51a01d85634408797223a
Gabriel Pulga <gabrieelplg@gmail.com>
2020-08-12 17:38:26 -0300

    day 4

---
+++ mobile/src/pages/Favorites/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View} from 'react-native';
+import styles from './styles';
+import PageHeader from '../../components/PageHeader';
+
+function Favorites() {
+    return (
+        <View style={styles.container}>
+        <PageHeader title="Meus proffys favoritos"/>
+      </View>
+    )
+}
+
+export default Favorites;
---
+++ mobile/src/pages/TeacherList/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View } from "react-native";
+import styles from "./styles";
+import PageHeader from "../../components/PageHeader";
+
+function TeacherList() {
+  return (
+    <View style={styles.container}>
+      <PageHeader title="Proffys disponíveis"/>
+    </View>
+  );
+}
+
+export default TeacherList;
